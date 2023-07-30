8e8b659fe3e2eec6f8910c75f873a18b6ee09636
Marcin Sapierzyński <m.sapierzynski@hotmail.com>
2023-07-30 12:54:56 +0200

    style

--- src/js/AboutMe.js
+++ src/js/AboutMe.js
@@ -4,66 +4,26 @@ import developper from "../img/developper.svg";
 
 const AboutMe = () => {
 
-    const skillsArray = ['JavaScript ES5/ES6+', 'Firebase', 'React', 'Bootstrap','HTML', 'CSS', 'Sass', 'Git', 'RWD', 'Json', 'Gulp', 'Github', 'English B2', 'Russian B2', 'Polish Native' ]
+    const skillsArray = ['JavaScript ES5/ES6+', 'Firebase', 'React','HTML', 'CSS', 'Sass', 'Git', 'RWD', 'Json', 'Gulp', 'Github', 'English B2', 'Russian B2', 'Polish Native' ]
 
     return (
     <div className="aboutMe">
         <section className="aboutMe_image">
             <div className="aboutMe_image_img"></div>
         </section>
-        <section className="aboutMe_container"
-        style={{
-            width: "100vw",
-        }}>
-            <div className="aboutMe_skills"
-            >
+        <section className="aboutMe_container">
+            <div className="aboutMe_skills">
                 <div>
                     <div className="aboutMe_skillImage"
-                    style={{
-                        backgroundImage: `url(${devices})`,
-                        height: "30vw",
-                        maxHeight: "283px",
-                        backgroundRepeat: "no-repeat",
-                        backgroundSize: "contain",
-                        backgroundPosition: "center",
-                        marginBottom: "0.1rem",
-                    }}
-                    >
+                        style={{backgroundImage: `url(${devices})`}}>
                     </div>
                 </div>
-                
-                <div className="aboutMe_skillsText" id="about"
-                style={{
-                    textAlign: "center",
-                    backgroundColor: "rgb(var(--color-blue))",
-                    width: "100vw",
-                    paddingTop: "5rem",
-                    paddingBottom: "13rem",
-                    display:"flex",
-                    justifyContent: "center",
-                    letterSpacing: "0.11rem",
-                    lineHeight: "1.5rem",
-
-                }}>
-                    <div 
-                    style={{
-                        width: "64%",
-                        color: "rgb(var(--color-white))",
-                    }}>
-                        <h1 className="aboutMe_skillsText_title"  
-                        style={{
-                            fontSize:"2.1rem",
-                            lineHeight:"3rem",
-                            fontWeight:"500",
-                            paddingBottom:"4rem"
-                        }}>
+                <div className="aboutMe_skillsText" id="about">
+                    <div>
+                        <h1 className="aboutMe_skillsText_title">
                             Hi, I'm Marcin. Nice to meet you.
                             </h1>
-                        <h5 className="aboutMe_skillsText_title_text"
-                        style={{
-                            fontSize:"1.2rem",
-                            paddingBottom:"3rem"
-                        }}>
+                        <h5 className="aboutMe_skillsText_title_text">
                         A highly motivated Front End Developer, seeking for new opportunity 
                         to start my career as a web developer! Able to create responsive, modern, 
                         and user-friendly applications and web pages using mobile first approach. 
@@ -73,81 +33,27 @@ const AboutMe = () => {
                     </div>
                 </div>
 
-                <div className="aboutMe_skillsBox"
-                style={{
-                    displey: "flex",
-                    paddingTop: "5rem"
-                }}>
-                    <div
-                    style={{
-                        width: "80%",
-                        border: "1px solid rgb(var(--color-grey209))",
-                        borderRadius: "1rem",
-                        margin: "auto",
-                        transform: "translate(0, -30%)",
-                        backgroundColor:"rgb(var(--color-white))",
-                        paddingTop: "5rem"
-
-                    }}>
+                <div className="aboutMe_skillsBox">
+                    <div className="aboutMe_skillsBox_container">
                         <section>
-                        <div className="aboutMe_skillsBox_logo"
-                            style={{
-                                height: "6rem"
-                            }}
-                        >
-                            <div
-                            style={{
-                                backgroundImage:`url(${developper})`,
-                                height: "5rem",
-                                backgroundRepeat: "no-repeat",
-                                backgroundSize: "contain",
-                                backgroundPosition: "center",
-                            }}></div>
+                        <div className="aboutMe_skillsBox_logo">
+                            <div style={{backgroundImage:`url(${developper})`}}>
+                            </div>
                         </div>
-                        <h2 className="aboutMe_skillsBox_position"
-                        style={{
-                            textAlign: "center",
-                            padding:"1rem 2rem 3rem 2rem",
-                            fontSize:"1.7rem",
-                            fontWeight:"500",
-                            
-                        }}>
+                        <h2 className="aboutMe_skillsBox_position">
                             Front-end Developer
                         </h2>
-                        <h3
-                        style={{
-                            textAlign: "center",
-                            paddingBottom:"2rem",
-                            fontSize:"1.3rem",
-                            padding:"0 3rem 4rem 3rem",
-
-                        }}>I like to code things from scratch, 
+                        <h3 className="aboutMe_skillsBox_text">
+                            I like to code things from scratch, 
                             and enjoy bringing ideas to life in the browser.
                         </h3>
                         </section>
-                        <section
-                        style={{
-                            displey: "flex",
-                            justifyContent: "center",
-                            textAlign: "center"
-                        }}>
-                            <div className="skills">
-                                <h3
-                                style={{
-                                    color:"rgb(var(--color-green))",
-                                    fontWeight:"500",
-                                    fontSize:"1.7rem",
-                                    paddingBottom: "2rem"
-                                }}>
+                        <section>
+                            <div className="aboutMe_skillsBox_skills">
+                                <h3>
                                     Skills
                                 </h3>
-                                <ul className="row"
-                                style={{
-                                    justifyContent:"center",
-                                    padding:"2rem",
-                                    fontSize:"1.3rem",
-                                    lineHeight:"1.5rem"
-                                }}>
+                                <ul className="row">
                                     {skillsArray.map((el)=> {
                                         return(
                                             <li className="col-" key={el}>{el}</li>
--- src/js/Contact.js
+++ src/js/Contact.js
@@ -4,7 +4,18 @@ const Contact = () => {
 
     const[formStatus, setFormStatus] = useState('Send')
     const [isDisable, setIsDisable] = useState(false)
+    const [form, setForm] = useState({
+        name: '',
+        email: '',
+        message: '',
+    })
 
+    const updateField = e => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        })
+    }
     const boxStyle = {
         boxShadow: isDisable ? 'none' : '',
     }
@@ -13,41 +24,33 @@ const Contact = () => {
         e.preventDefault()
         setFormStatus('Sended')
         setIsDisable(!isDisable)
-        const {name, email, message} = e.target.elements
-        let conFom = {
-            name: name.value,
-            email: email.value,
-            message: message.value,
-        }
-        console.log(conFom)
+        
+        // const {name, email, message} = e.target.elements
+        // let conFom = {
+        //     name: name.value,
+        //     email: email.value,
+        //     message: message.value,
+        // }
+        // console.log(conFom)
     }
 
     return (
-        <section className="contact_container" id="contact"
-        style={{
-            backgroundColor: "rgb(var(--color-blue))",
-            display:"flex",
-            justifyContent:"center",
-            alignItems:"center",
-            flexDirection:"column",
-            padding:"6rem",
-            color:"rgb(var(--color-white))",
-        }}>
+        <section className="contact_container" id="contact">
             <form className="form" onSubmit={onSubmit} >
-            <h2
-            style={{
-                paddingBottom:"1.5rem",
-                fontSize:"1.5rem",
-                fontWeight:"500"
-            }}>Contact me</h2>
+            <h2 className="contact_container_text">
+                Contact me
+            </h2>
                 <div className="form_line">
-                    <input className="form_line_input" type="text" id="name" placeholder="Type your name" required/>
+                    <input className="form_line_input" type="text" id="name" placeholder="Type your name" name="name" required
+                    onChange={updateField}/>
                 </div>
                 <div className="form_line">
-                    <input className="form_line_input" type="text" id="email" placeholder="Type your e-mail" required/>
+                    <input className="form_line_input" type="text" id="email" placeholder="Type your e-mail" name="email" required
+                    onChange={updateField}/>
                 </div>
                 <div className="form_line form_text">
-                    <textarea className="form_line_input" type="text" id="message" placeholder="Type your message" required/>
+                    <textarea className="form_line_input" type="text" id="message" placeholder="Type your message" name="message" required
+                    onChange={updateField}/>
                 </div>
                 <button className="btn btnSmall" type="submit" disabled={isDisable}
                     style={boxStyle}>
